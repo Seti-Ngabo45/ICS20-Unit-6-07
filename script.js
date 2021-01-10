7aba4e7ab903399bdf1ca27623a9520fa1a8a801
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-10 02:06:46 +0000

    L15

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('answer').addEventListener('click',multiply)
 
 function multiply() {
   let counter = document.getElementById('counter').value;
-  let x = document.getElementById('x').value;
+  let number = document.getElementById('x').value;
   x = parseInt(x);
   let answer = 0;
   while (counter > 0) {
