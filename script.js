1534ad5b740443b2cff7b08972ddada37b80f20f
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-10 01:27:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('answer').addEventListener('click',multiply)
+
+function multiply() {
+  let counter = document.getElementById('counter').value;
+  let x = document.getElementById('x').value;
+  x = parseInt(x);
+  let answer = 0;
+  while (counter > 0) {
+    counter--   
+    answer = answer + x
+  }
+  alert(answer);
+}
+    
